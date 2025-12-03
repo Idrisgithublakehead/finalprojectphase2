9a3fd50c21ecce0640502c9f2c9c8fbd64363550
Idris Zahir <izahir@lakeheadu.ca>
2025-12-02 19:01:34 -0500

    feat: added footer.php with layout and Bootstrap scripts

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+</main>
+
+<footer class="bg-light text-center py-3 mt-5 border-top">
+    <small>&copy; <?php echo date("Y"); ?> - IMS Project</small>
+    <!-- echo date format above for bottom of page -->
+</footer>
+
+<!-- Bootstrap script below -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
